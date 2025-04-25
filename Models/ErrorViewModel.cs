5be366415cef585128856cdb1e1bbf3ed9f2c79b
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-04-25 15:16:31 -0300

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03_MUSICSTORE.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
